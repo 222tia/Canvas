28bca20584a1063e05d44b7465eb436b86e80f3d
Tia Self <tiamarie.self@bsu.edu>
2024-01-24 19:36:51 -0500

    add assignments class

---
+++ DomainModel/Assignments.cs
@@ -0,0 +1,15 @@
+namespace DomainModel;
+
+public class Assignments
+{
+    protected string assignmentName;
+    private string assignmentInfo;
+    private int dueDate;
+    private int pointsValue;
+    private void submit() {
+
+    }
+    private void upload() {
+        
+    }
+}
