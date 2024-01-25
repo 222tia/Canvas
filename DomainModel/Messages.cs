de105d743f77de5efd8e6547cc53410765634a76
Tia Self <tiamarie.self@bsu.edu>
2024-01-24 19:33:07 -0500

    add messages class

---
+++ DomainModel/Messages.cs
@@ -0,0 +1,10 @@
+namespace DomainModel;
+
+public class Messages
+{
+    private string sender;
+    private List<string> recipients;
+    private int dateSent;
+    protected string messageHeader;
+    private string messageContent;
+}
