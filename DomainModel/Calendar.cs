4b55c8e9fb6adadaedf05f7642abbb1651f53f2a
Tia Self <tiamarie.self@bsu.edu>
2024-01-24 19:34:35 -0500

    add calendar class

---
+++ DomainModel/Calendar.cs
@@ -0,0 +1,18 @@
+namespace DomainModel;
+
+public class Calendar
+{
+    private string month;
+    private int day;
+    private int year;
+    private List<string> calendars;
+    private void editEvent() {
+
+    }
+    private void deleteEvent() {
+
+    }
+    private void addEvent() {
+        
+    }
+}
