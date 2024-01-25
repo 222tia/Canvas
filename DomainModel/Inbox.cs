d404918775b8f6184b339849eb741b044d699a30
Tia Self <tiamarie.self@bsu.edu>
2024-01-24 19:26:45 -0500

    add Inbox class

---
+++ DomainModel/Inbox.cs
@@ -0,0 +1,23 @@
+namespace DomainModel;
+
+public class Inbox
+{
+    private int totalMessages;
+    private List<string> messages;
+
+    private void searchMessages() {
+
+    }
+
+    private void searchPeople() {
+
+    }
+
+    private void sendMessage() {
+
+    }
+
+    private void deleteMessage() {
+        
+    }
+}
